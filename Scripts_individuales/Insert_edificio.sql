3facd39328d4196122fcc76d4ea933940e48051f
AllanEstrada <82833680+DRAC-09@users.noreply.github.com>
2021-04-28 20:44:54 -0600

    Update Insert_edificio.sql

--- Scripts_individuales/Insert_edificio.sql
+++ Scripts_individuales/Insert_edificio.sql
@@ -1,6 +1,6 @@
-INSERT INTO `miprimeradb`.`edificio` (`cod_edificio`, `nombre`) VALUES ('A1', 'Ciencias Sociales');
-INSERT INTO `miprimeradb`.`edificio` (`cod_edificio`, `nombre`) VALUES ('A2', 'Ciencias Jurídicas');
-INSERT INTO `miprimeradb`.`edificio` (`cod_edificio`, `nombre`) VALUES ('B2', 'Ingeniería');
-INSERT INTO `miprimeradb`.`edificio` (`cod_edificio`, `nombre`) VALUES ('C1', 'Ciencias Económicas');
-INSERT INTO `miprimeradb`.`edificio` (`cod_edificio`, `nombre`) VALUES ('J1', 'Ciencias Biológicas');
-INSERT INTO `miprimeradb`.`edificio` (`cod_edificio`, `nombre`) VALUES ('K1', 'Ciencias Espaciales');
+INSERT INTO edificio VALUES ('A1', 'Ciencias Sociales');
+INSERT INTO edificio VALUES ('A2', 'Ciencias Jurídicas');
+INSERT INTO edificio VALUES ('B2', 'Ingeniería');
+INSERT INTO edificio VALUES ('C1', 'Ciencias Económicas');
+INSERT INTO edificio VALUES ('J1', 'Ciencias Biológicas');
+INSERT INTO edificio VALUES ('K1', 'Ciencias Espaciales');
