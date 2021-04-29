2f0f5bbd850f0253cf45146baddca5273800c44f
AllanEstrada <82833680+DRAC-09@users.noreply.github.com>
2021-04-28 19:39:24 -0600

    Add files via upload

---
+++ Scripts_individuales/Insert_edificio.sql
@@ -0,0 +1,8 @@
+SELECT * FROM edificio;
+
+INSERT INTO `miprimeradb`.`edificio` (`cod_edificio`, `nombre`) VALUES ('A1', 'Ciencias Sociales');
+INSERT INTO `miprimeradb`.`edificio` (`cod_edificio`, `nombre`) VALUES ('A2', 'Ciencias Jurídicas');
+INSERT INTO `miprimeradb`.`edificio` (`cod_edificio`, `nombre`) VALUES ('B2', 'Ingeniería');
+INSERT INTO `miprimeradb`.`edificio` (`cod_edificio`, `nombre`) VALUES ('C1', 'Ciencias Económicas');
+INSERT INTO `miprimeradb`.`edificio` (`cod_edificio`, `nombre`) VALUES ('J1', 'Ciencias Biológicas');
+INSERT INTO `miprimeradb`.`edificio` (`cod_edificio`, `nombre`) VALUES ('K1', 'Ciencias Espaciales');
