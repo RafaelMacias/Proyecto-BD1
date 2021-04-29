c7b223f26034e266fbc346660f4a0326f0f74872
AllanEstrada <82833680+DRAC-09@users.noreply.github.com>
2021-04-28 19:40:54 -0600

    Add files via upload

---
+++ Scripts_individuales/Insert_aula.sql
@@ -0,0 +1,14 @@
+SELECT * FROM aula;
+
+INSERT INTO `miprimeradb`.`aula` (`cod_aula`, `cod_edificio`, `capacidad`) VALUES ('A1-101', 'A1', '50');
+INSERT INTO `miprimeradb`.`aula` (`cod_aula`, `cod_edificio`, `capacidad`) VALUES ('A1-202', 'A1', '40');
+INSERT INTO `miprimeradb`.`aula` (`cod_aula`, `cod_edificio`, `capacidad`) VALUES ('A2-101', 'A2', '50');
+INSERT INTO `miprimeradb`.`aula` (`cod_aula`, `cod_edificio`, `capacidad`) VALUES ('A2-202', 'A2', '40');
+INSERT INTO `miprimeradb`.`aula` (`cod_aula`, `cod_edificio`, `capacidad`) VALUES ('B2-101', 'B2', '50');
+INSERT INTO `miprimeradb`.`aula` (`cod_aula`, `cod_edificio`, `capacidad`) VALUES ('B2-201', 'B2', '40');
+INSERT INTO `miprimeradb`.`aula` (`cod_aula`, `cod_edificio`, `capacidad`) VALUES ('C1-101', 'C1', '50');
+INSERT INTO `miprimeradb`.`aula` (`cod_aula`, `cod_edificio`, `capacidad`) VALUES ('C1-201', 'C1', '40');
+INSERT INTO `miprimeradb`.`aula` (`cod_aula`, `cod_edificio`, `capacidad`) VALUES ('J1-101', 'J1', '50');
+INSERT INTO `miprimeradb`.`aula` (`cod_aula`, `cod_edificio`, `capacidad`) VALUES ('J1-201', 'J1', '40');
+INSERT INTO `miprimeradb`.`aula` (`cod_aula`, `cod_edificio`, `capacidad`) VALUES ('K1-101', 'K1', '50');
+INSERT INTO `miprimeradb`.`aula` (`cod_aula`, `cod_edificio`, `capacidad`) VALUES ('K1-201', 'K1', '40');
