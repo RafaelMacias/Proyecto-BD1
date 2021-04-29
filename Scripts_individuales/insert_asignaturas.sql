168307f30f2c44cd6f34d4890859b503b47fcc35
RafaelMacias <39977339+RafaelMacias@users.noreply.github.com>
2021-04-28 21:24:14 -0600

    insert asignaturas

---
+++ Scripts_individuales/insert_asignaturas.sql
@@ -0,0 +1,9 @@
+insert into asignatura values('MM-110','IS','Matematicas 1','5');
+insert into asignatura values('MM-111','IS','Geometria','5');
+insert into asignatura values('MM-314','IS','Programacion 1','5');
+insert into asignatura values('CDE-022','CD','Etica General','3');
+insert into asignatura values('CDE-032','CD','Logica Juridica','3');
+insert into asignatura values('CDE-042','CD','Interpretacion Juridica','3');
+insert into asignatura values('CE040','CA','Microeconomia','3');
+insert into asignatura values('CF035','CA','Contabilidad II','4');
+insert into asignatura values('CE075','CA','Macroeconomia','4');
