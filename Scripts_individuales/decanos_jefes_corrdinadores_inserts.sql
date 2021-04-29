42fdee0dd4fbe722ba8b2f824a1deb270ef62103
WilliamGar1 <57337849+WilliamGar1@users.noreply.github.com>
2021-04-28 19:49:40 -0600

    inserts de decanos, jefes y coordinadores

---
+++ Scripts_individuales/decanos_jefes_corrdinadores_inserts.sql
@@ -0,0 +1,11 @@
+insert into decano values('1','0301-1980-02584');
+insert into decano values('2','0401-1975-01254');
+insert into decano values('3','1810-1987-01584');
+
+insert into jefe_carrera values('1','0302-1981-00254');
+insert into jefe_carrera values('2','0801-1988-12345');
+insert into jefe_carrera values('3','0301-1988-02554');
+
+insert into coord_carrera values('1','0101-1985-58749');
+insert into coord_carrera values('2','0602-1989-12458');
+insert into coord_carrera values('3','1309-1983-25680');
