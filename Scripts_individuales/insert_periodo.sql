f7d473f9f7cbea2fb00193fead87e8de4a66e6b5
RafaelMacias <39977339+RafaelMacias@users.noreply.github.com>
2021-04-28 20:41:36 -0600

    insert de periodo

---
+++ Scripts_individuales/insert_periodo.sql
@@ -0,0 +1,13 @@
+use miprimeradb;
+insert into periodo values('01','1','2015-01-01','2015-01-01','2015-01-03');
+insert into periodo values('02','2','2015-01-01','2015-01-03','2015-01-07');
+insert into periodo values('03','3','2015-01-01','2015-01-07','2015-01-10');
+insert into periodo values('04','1','2016-01-01','2016-01-01','2016-01-03');
+insert into periodo values('05','2','2016-01-01','2016-01-03','2016-01-07');
+insert into periodo values('06','3','2016-01-01','2016-01-07','2016-01-10');
+insert into periodo values('07','1','2017-01-01','2017-01-01','2017-01-03');
+insert into periodo values('08','2','2017-01-01','2017-01-03','2017-01-07');
+insert into periodo values('09','3','2017-01-01','2017-01-07','2017-01-10');
+insert into periodo values('10','1','2018-01-01','2018-01-01','2018-01-03');
+insert into periodo values('11','2','2018-01-01','2018-01-03','2018-01-07');
+insert into periodo values('12','3','2018-01-01','2018-01-07','2018-01-10');
