00ea7bde4d3a45d50e5e9cc73136952bfbe9ae69
AllanEstrada <82833680+DRAC-09@users.noreply.github.com>
2021-04-28 20:48:52 -0600

    Update Insert_facultad.sql

--- Scripts_individuales/Insert_facultad.sql
+++ Scripts_individuales/Insert_facultad.sql
@@ -1,3 +1,3 @@
-INSERT INTO `miprimeradb`.`facultad` (`cod_facultad`, `nombre`, `cod_edificio`, `id_decano`) VALUES ('FI', 'Facultad de Ingenieria', 'B2', '1');
-INSERT INTO `miprimeradb`.`facultad` (`cod_facultad`, `nombre`, `cod_edificio`, `id_decano`) VALUES ('FCJ', 'Facultad de Ciencias Juridicas', 'A2', '2');
-INSERT INTO `miprimeradb`.`facultad` (`cod_facultad`, `nombre`, `cod_edificio`, `id_decano`) VALUES ('FCE', 'Facultad de Ciencias Economicas', 'C1', '3');
+INSERT INTO facultad VALUES ('FI', 'Facultad de Ingenieria', 'B2', '1');
+INSERT INTO facultad VALUES ('FCJ', 'Facultad de Ciencias Juridicas', 'A2', '2');
+INSERT INTO facultad VALUES ('FCE', 'Facultad de Ciencias Economicas', 'C1', '3');
