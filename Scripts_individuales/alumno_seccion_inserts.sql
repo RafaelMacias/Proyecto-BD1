cb93ffdaa6685028b334bdce033418bfbba5135e
WilliamGar1 <57337849+WilliamGar1@users.noreply.github.com>
2021-04-29 19:45:04 -0600

    Add files via upload

---
+++ Scripts_individuales/alumno_seccion_inserts.sql
@@ -0,0 +1,7 @@
+insert into alumno_seccion values('20041598746','7','70');
+insert into alumno_seccion values('20071549684','7','80');
+insert into alumno_seccion values('20071685468','1','64');
+insert into alumno_seccion values('20081215465','1','90');
+insert into alumno_seccion values('20091589544','2','46');
+insert into alumno_seccion values('20095843135','2','78');
+insert into alumno_seccion values('20095843135','3','78');
